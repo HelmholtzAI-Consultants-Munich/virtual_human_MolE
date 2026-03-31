46e99023c094899ac9f3f24e82934876d99550e6
Maksim Pavlov <maksim.pavlov@tum.de>
2026-03-31 08:57:41 +0200

    package update

--- mole_package/src/mole_package/__init__.py
+++ mole_package/src/mole_package/__init__.py
@@ -1,5 +1,5 @@
 from .ginet_concat import GINet
 #from .dataset_representation_old import batch_representation
-from .data_representation import batch_representation
+from .dataset_representation import batch_representation
 from .mole_representation import read_smiles
 from .mole_antimicrobial_prediction import add_strains
